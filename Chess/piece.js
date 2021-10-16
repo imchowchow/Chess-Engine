11c2eac221f5595c62da297940a5e7491e2f1a5d
Larson Patete <51838778+imchowchow@users.noreply.github.com>
2021-10-15 19:00:04 -0500

    Added movement rules for knight, bishop, queen, and king

--- Chess/piece.js
+++ Chess/piece.js
@@ -8,11 +8,15 @@ class Piece {
         this.file = parseInt(tile / 8, 10); // y
         this.rank = tile % 8; // x
         this.xPos = this.rank * tileWidth;
-        this.yPos = this.file * tileHeight; 
+        this.yPos = this.file * tileHeight;
+        
         this.width = tileWidth;
         this.height = tileHeight;
 
+        this.moveLst;
+    }
 
+    resetMoves() {
         this.moveLst = [
             [0, 0, 0, 0, 0, 0, 0, 0], // put zeros so its easier to debug
             [0, 0, 0, 0, 0, 0, 0, 0],
@@ -65,7 +69,7 @@ class Piece {
         ctx.fillRect(this.xPos, this.yPos, tileWidth, tileHeight);
     }
 
-    addHorizontalMoves(board, tile, increase, edge) {
+    AddSlidingMoves(board, tile, increase, edge) {
         let step = (increase > 0) ? tile : tile * -1;
         while (step <= edge) {
             let file = parseInt(Math.abs(step) / 8, 10);
@@ -85,6 +89,53 @@ class Piece {
             step += Math.abs(increase);
         }
     }
+
+    straightMoves(board) {
+        let southEdge = ((totRows - this.file) * 8) + this.tile; // the end of the board for a direction from a specific square
+        let northEdge = (this.tile % (8 * this.file)) * -1;
+        let eastEdge = this.tile + (totRows - this.rank);
+        let westEdge = (this.tile - this.rank) * -1;
+
+        this.AddSlidingMoves(board, this.tile, 1, eastEdge);
+        this.AddSlidingMoves(board, this.tile, -1, westEdge);
+        this.AddSlidingMoves(board, this.tile, 8, southEdge);
+        this.AddSlidingMoves(board, this.tile, -8, northEdge);
+    }
+
+    diagonalMoves(board) {
+        let southWestEdge = this.tile + (Math.min(this.rank, totRows - this.file) * 7)
+        let southEastEdge = this.tile + (Math.min(totRows - this.rank, totRows - this.file) * 9)
+        let northWestEdge = (this.tile - (Math.min(this.rank, this.file) * 9)) * -1;
+        let northEastEdge = (this.tile - (Math.min(totRows - this.rank, this.file) * 7)) * -1;
+
+        this.AddSlidingMoves(board, this.tile, 7, southWestEdge);
+        this.AddSlidingMoves(board, this.tile, 9, southEastEdge);
+        this.AddSlidingMoves(board, this.tile, -9, northWestEdge);
+        this.AddSlidingMoves(board, this.tile, -7, northEastEdge);
+    }
+
+    checkMoves(board, lst) {
+        for (let x = 0; x < lst.length; x++) {
+            let newTile = this.tile + lst[x];
+            if (newTile > 63 || newTile < 0) {
+                continue;
+            }
+            let file = parseInt(Math.abs(newTile) / 8, 10);
+            let rank = Math.abs(newTile) % 8;
+            if (board.board[file][rank] === this) {
+                this.moveLst[file][rank] = 1;
+            } else if (board.board[file][rank] == null) {
+                this.moveLst[file][rank] = 1;
+            } else if (board.board[file][rank].color != this.color) {
+                this.moveLst[file][rank] = 1;
+                continue;
+            } else if (board.board[file][rank].color == this.color) {
+                continue;
+            } else {
+                this.moveLst[file][rank] = 0;
+            }
+        }
+    }
 }
 
 class King extends Piece {
@@ -100,6 +151,12 @@ class King extends Piece {
         ctx.drawImage(pic, this.xPos - 138, this.yPos - 68, canvas.width / 2.14, canvas.height / 3.42); // x and y weird cuz canvas
 
     }
+
+    moves(board) {
+        super.resetMoves();
+        this.increments = [0, 1, -1, 7, -7, 8, -8, 9, -9];
+        super.checkMoves(board, this.increments);
+    }
 }
 
 class Queen extends Piece {
@@ -113,6 +170,12 @@ class Queen extends Piece {
         const pic = document.getElementById(this.color + ' Queen');
         ctx.drawImage(pic, this.xPos - 140, this.yPos - 62, canvas.width / 2, canvas.height / 3.42); // x and y weird cuz canvas
 
+    }
+
+    moves(board) {
+        super.resetMoves();
+        super.diagonalMoves(board);
+        super.straightMoves(board);
     }
 }
 
@@ -128,6 +191,16 @@ class Bishop extends Piece {
         ctx.drawImage(pic, this.xPos - 137, this.yPos - 62, canvas.width / 2.13, canvas.height / 3.42); // x and y weird cuz canvas
 
     }
+
+    showMoves(board) {
+        this.moves(board);
+        // board.changeColor();
+    }
+
+    moves(board) {
+        super.resetMoves();
+        super.diagonalMoves(board);
+    }
 }
 
 class Knight extends Piece {
@@ -141,6 +214,12 @@ class Knight extends Piece {
         const pic = document.getElementById(this.color + ' Knight');
         ctx.drawImage(pic, this.xPos - 137, this.yPos - 58, canvas.width / 2.13, canvas.height / 3.42); // x and y weird cuz canvas
 
+    }
+
+    moves(board) {
+        super.resetMoves();
+        this.increments = [0, 10, -10, 17, -17, 15, -15, 6, -6];
+        super.checkMoves(board, this.increments);
     }
 }
 
@@ -164,26 +243,8 @@ class Rook extends Piece {
     }
 
     moves(board) {
-        this.moveLst = [ 
-            [0, 0, 0, 0, 0, 0, 0, 0], 
-            [0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0]
-        ];
-
-        let southEdge = ((totRows - this.file) * 8) + this.tile; // the end of the board for a direction from a specific square
-        let northEdge = (this.tile % (8 * this.file)) * -1;
-        let eastEdge = this.tile + (totRows - this.rank);
-        let westEdge = (this.tile - this.rank) * -1;
-
-        this.addHorizontalMoves(board, this.tile, 1, eastEdge);
-        this.addHorizontalMoves(board, this.tile, -1, westEdge);
-        this.addHorizontalMoves(board, this.tile, 8, southEdge);
-        this.addHorizontalMoves(board, this.tile, -8, northEdge);
+        super.resetMoves();
+        super.straightMoves(board);    
     }
 }
 
